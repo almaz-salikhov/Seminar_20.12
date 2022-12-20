bdf8c644ac046c3aeccf57139ca2ce1137ed5d1d
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-20 21:44:20 +0200

    Seminar' tasks done

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,6 @@
+﻿int Fibonachi(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
---
+++ task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задать двумерный массив Найти элементы с четными индексами (оба)
+// И заменить их элементы на их квадраты 
+
+// 1) создаем массив
+void FillArray(int[,] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++) // внешний цикл - выбор строки 
+    {
+        for (int j = 0; j < arr.GetLength(1); j++) // внутренний цикл заполняет строку
+        {
+            arr[i, j] = rnd.Next(2, 5);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void ChangeElementsToSquare(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; i < arr.GetLength(1); i++)
+        {
+            if (i % 2 == 0 & j % 2 == 0)
+            {
+                arr[i, j] *= arr[i, j]; // умножение с накоплением, умножение на себя
+            }
+        }
+    }
+}
+
+int rowCount = 3;
+int columnCount = 4;
+int[,] array = new int[rowCount, columnCount];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeElementsToSquare(array);
+PrintArray(array);
+
