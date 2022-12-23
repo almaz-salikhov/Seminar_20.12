e6826e6e77e3d50f3eb1e5f06a68b8c7432887de
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-23 18:40:12 +0200

    HW task 2 done

---
+++ HomeWork/task2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая на вход принимает позиции (два индекса) элемента в двумерном массиве
+// возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+void FillArray(int[,] arr)
+{
+    Random rnd = new Random();
+        for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(1, 10);
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArray(int[,] arr)
+{
+    Console.WriteLine("Полученный массив: ");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void ShowElementIfExist(int[,] arr, int row, int column)
+{
+    if (row <= arr.GetLength(0) & column <= arr.GetLength(1))
+    {
+        Console.WriteLine("Искомый элемент: " + 
+        (arr[arr.GetLength(0) - 1, arr.GetLength(1) - 1]));
+    }
+    else
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+    }
+}
+
+Console.WriteLine("Введите количество строк нового массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов нового массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Введите номер строки искомого элемента: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца искомого элемента: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+ShowElementIfExist(array, M, N);
